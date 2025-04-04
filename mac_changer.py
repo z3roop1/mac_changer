e155436ccb66859655e01494d73a0375e5cea803
sagar <roopsagartella99@gmail.com>
2025-04-04 22:07:06 +0530

    this is a secure way of code - no command injection

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 
+#list of commands to be used to change a mac address of a machine
 #ifconfig wlan0 down
 #ifconfig wlan0 hw ether 01:11:12:23:32:44
 #ifconfig wlan0 up
@@ -10,6 +11,7 @@ interface = input("enter the interface > ")
 new_mac = input("enter the new MAC address > ")
 
 print("[+] Changing mac address")
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell= True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+#subprocess.call([]) is way more secure than subprocess.call(,shell=true)
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig",interface,"up"])
