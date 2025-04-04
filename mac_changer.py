711377c2a8dcfc5359f7f28f437324701d1da2b3
sagar <roopsagartella99@gmail.com>
2025-04-04 21:58:45 +0530

    added variables but insecure code - command injection

--- mac_changer.py
+++ mac_changer.py
@@ -5,7 +5,11 @@ import subprocess
 #ifconfig wlan0 down
 #ifconfig wlan0 hw ether 01:11:12:23:32:44
 #ifconfig wlan0 up
+
+interface = input("enter the interface > ")
+new_mac = input("enter the new MAC address > ")
+
 print("[+] Changing mac address")
-subprocess.call("ifconfig wlp4s0 down" , shell = True)
-subprocess.call("ifconfig wlp4s0 hw ether 12:23:34:45:23:65", shell= True)
-subprocess.call("ifconfig wlp4s0 up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell= True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
