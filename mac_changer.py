27e44a5fc007122dda7ef1192943867d31ba42b2
sagar <roopsagartella99@gmail.com>
2025-04-04 22:46:22 +0530

    added argument parser: argparse

--- mac_changer.py
+++ mac_changer.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 
 import subprocess
+import argparse
 
 #list of commands to be used to change a mac address of a machine
 #ifconfig wlan0 down
 #ifconfig wlan0 hw ether 01:11:12:23:32:44
 #ifconfig wlan0 up
 
-interface = input("enter the interface > ")
-new_mac = input("enter the new MAC address > ")
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--interface", help="add the Interface(ex: wlan0)",type=str, required=True)
+parser.add_argument("-m", "--mac", help="add a new MAC address(ex: 00:11:22:33:44:55)",type=str, required=True)
+args = parser.parse_args()
+
 
 print("[+] Changing mac address")
 #subprocess.call([]) is way more secure than subprocess.call(,shell=true)
-subprocess.call(["ifconfig",interface,"down"])
-subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
-subprocess.call(["ifconfig",interface,"up"])
+subprocess.call(["ifconfig",args.interface,"down"])
+subprocess.call(["ifconfig",args.interface,"hw","ether",args.mac])
+subprocess.call(["ifconfig",args.interface,"up"])
