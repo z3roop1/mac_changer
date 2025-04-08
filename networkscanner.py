ba2f2ddb717de87e2a008193d3477cf25b2aaaa0
sagar <roopsagartella99@gmail.com>
2025-04-08 21:50:09 +0530

    created arp packet,broadcast and combined them and summary()

--- networkscanner.py
+++ networkscanner.py
@@ -1,6 +1,21 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip)
+    #this is to make an arp packet 
+    arp_request = scapy.ARP(pdst=ip)
+    print(arp_request.summary())
 
-scan("192.168.1.1/24")
+    #this is set an MAC address
+    #* as data in a network is shared through a MAC address not IP address
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    print(broadcast.summary())
+
+    #Combining the both arp and broadcast
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request_broadcast.summary())
+
+    #scapy.ls is used for to list information about the classes 
+    # scapy.ls(scapy.ARP())
+    # scapy.ls(scapy.Ether())
+
+scan("192.168.1.9")
