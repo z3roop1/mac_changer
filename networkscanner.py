910a88ea7b87626a13a338a4e74992b9e177f8d8
sagar <roopsagartella99@gmail.com>
2025-04-08 22:08:38 +0530

    sending and collecting the packet using scapy.srp

--- networkscanner.py
+++ networkscanner.py
@@ -1,21 +1,21 @@
 import scapy.all as scapy
 
 def scan(ip):
-    #this is to make an arp packet 
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
-
-    #this is set an MAC address
-    #* as data in a network is shared through a MAC address not IP address
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    print(broadcast.summary())
-
-    #Combining the both arp and broadcast
     arp_request_broadcast = broadcast/arp_request
-    print(arp_request_broadcast.summary())
+    #srp is used to send and recive packets we can do the same with sr 
+    #but with srp we can also set the custom Headers(here MAC address)
+    answered  = scapy.srp(arp_request_broadcast, timeout=1)[0]
 
-    #scapy.ls is used for to list information about the classes 
-    # scapy.ls(scapy.ARP())
-    # scapy.ls(scapy.Ether())
+    for element in answered:
+        print(element)
+        # print(element[1].hwdst)
+        # print(element[1].pdst)
+        print("--------------------------------------------")
 
-scan("192.168.1.9")
+
+scan("192.168.1.9/24")
+
+#for this code this is output it contains sent raw packet and response raw packet
+#QueryAnswer(query=<Ether  dst=ff:ff:ff:ff:ff:ff type=ARP |<ARP  pdst=192.168.1.23 |>>, answer=<Ether  dst=30:c9:ab:8b:96:0f src=7e:58:26:25:0a:de type=ARP |<ARP  hwtype=Ethernet (10Mb) ptype=IPv4 hwlen=6 plen=4 op=is-at hwsrc=7e:58:26:25:0a:de psrc=192.168.1.23 hwdst=30:c9:ab:8b:96:0f pdst=192.168.1.24 |<Padding  load='\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' |>>>)
